beee1938f9b84408776492a5705e86f54b69fb20
[Nate] <[ncggardner@gmail.com]>
2020-05-02 20:04:00 -0400

    Task List first commit

---
+++ TaskListCapstone/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListCapstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome to the Task Manager");
+            Console.WriteLine();
+
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("options Menu:\n(Please choose one)");
+                string userTask = GetUserInfo("1: List tasks\n2: Add task\n3: Delete task\n4: Mark task complete\n5: Quit");
+                Console.WriteLine();
+                if (userTask == "1")
+                {
+                    PrintListInfo();
+                }
+                else if (userTask == "2")
+                {
+                    string a = NewTaskName();
+                    string b = NewTaskDescrition();
+                    DateTime c = NewTaskDate();
+                    List<Task> taskList = new List<Task>
+                    {
+
+
+                    new Task(a, b, c)
+                    };
+                    PrintListInfo();
+
+
+
+                }
+                else if (userTask == "3")
+                {
+                    Console.WriteLine("please enter task number to delete: ");
+                    string userNum = Console.ReadLine().Trim();
+                    int numDelete = int.Parse(userNum);
+                    Console.WriteLine($"Are you sure you would like to delete task: {userNum}? y/n");
+                    string answer = Console.ReadLine().Trim();
+                    if(answer == "y")
+                    {
+                        Console.WriteLine("cool");
+                        //taskList.RemoveAt(numDelete - 1);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (userTask == "4")
+                {
+
+                }
+                else if (userTask == "5")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid entry");
+                }
+                
+            }
+            
+        }
+        public static string GetUserInfo(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine().Trim();
+            return input;
+        }
+        public static void PrintListInfo()
+        {
+            List<Task> taskList = Task.GetTaskList();
+
+            Console.WriteLine("\tTeam Member\tDue Date\tcompleted\tTask Description");
+            Console.WriteLine("\t___________\t________\t_________\t________________");
+            int i = 1;
+            foreach (Task todo in taskList)
+            {
+
+                Console.WriteLine($"{i}\t{todo.Name}\t{todo.DueDate.ToShortDateString()}\t{todo.Complete}\t\t{todo.Description}");
+                i++;
+            }
+        }
+        public static string NewTaskName()
+        {
+            Console.WriteLine("Who would you like to assign a task to?");
+            string person = Console.ReadLine().Trim();
+            return person;
+        }
+        public static string NewTaskDescrition()
+        {
+            Console.WriteLine("Please enter a brief description of the task");
+            string taskDescription = Console.ReadLine().Trim();
+            return taskDescription;
+        }
+        public static DateTime NewTaskDate()
+        {
+            Console.WriteLine("Please enter a due date (mm/dd/yyyy)");
+            string dateDue = Console.ReadLine();
+            DateTime date = DateTime.Parse(dateDue);
+            return date;
+        }
+
+
+    }
+}
---
+++ TaskListCapstone/Task.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskListCapstone
+{
+    class Task
+    {
+        private string name;
+        private string description;
+        private DateTime dueDate;
+        private bool complete;
+
+        #region properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public DateTime DueDate
+        {
+            get
+            {
+                return dueDate;
+            }
+            set
+            {
+                dueDate = value;
+            }
+        }
+        public bool Complete
+        {
+            get
+            {
+                return complete;
+            }
+            set
+            {
+                complete = value;
+            }
+        }
+        #endregion
+        #region constructors
+        public Task()
+        {
+
+        }
+        public Task(string _name, string _description, DateTime _dueDate)
+        {
+            name = _name;
+            description = _description;
+            dueDate = _dueDate;
+            complete = false;
+        }
+        public Task(string _name, string _description, DateTime _dueDate, bool _complete)
+        {
+            name = _name;
+            description = _description;
+            dueDate = _dueDate;
+            complete = _complete;
+        }
+        #endregion
+
+
+        public static List<Task> GetTaskList()
+        {
+            List<Task> taskList = new List<Task>
+            {
+                new Task("Robert Moon", "Make Coffee", DateTime.Parse("5/4/2020")),
+                new Task("Anna Baseley", "Drink Coffee", DateTime.Parse("5/4/2020")),
+                new Task("Johnny Doe", "Set up Zoom meeting for 9AM", DateTime.Parse("5/4/2020")),
+                new Task("The Brain", "Take over the World!", DateTime.Parse("1/1/3000")),
+                new Task("Nate Gardner", "Be Awesome!", DateTime.Parse("5/25/2020"), true),
+            };
+            return taskList;
+        }
+
+        
+
+        
+        
+    }
+}
